c97236bdd58efcc34880b29d0e923c35f6ba3777
Aravind830 <aravindsamy830@gmail.com>
2025-03-31 13:04:14 +0530

    first commit

---
+++ src/main/java/com/jenkins/jenkins/controller/JenkinsController.java
@@ -0,0 +1,21 @@
+package com.jenkins.jenkins.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+@RequestMapping("/student")
+public class JenkinsController {
+
+
+    @GetMapping("/all")
+    public ResponseEntity<String> getDetails(){
+        return ResponseEntity.ok("welcome to jenkins world");
+    }
+
+
+}
